48da18b65b882fd9a451332a40a3961ccbefd8cc
SultanAli41 <Sultanalibalouch512@gmail.com>
2026-03-02 06:23:32 +0500

    Functions

---
+++ functions.js
@@ -0,0 +1,38 @@
+//functions - we use funcion when we have to repeat code or reuse the code 
+function alphabets(){
+console.log("A");
+console.log("B");
+console.log("C");
+console.log("D");
+console.log("E");
+}
+alphabets();
+//----------
+// function statment
+function abcd(){
+
+}
+// function expresion
+let fnc = function(){
+    // fnc expresion
+    // now let's name is fnc's name 
+}
+// Fat arrow function
+let fnc1=()=>{
+
+}
+// Parameters & Arguments
+function dance (val){
+console.log(`${val} Naach raha hai`)
+}
+dance("ghora");
+dance("Kadha");
+dance("cheel");
+dance("kawa");
+function addition (v1 , v2){// parametrs
+    console.log(v1 + v2);
+}
+addition( 3,2);
+addition( 5,5);
+addition( 7,9);
+addition( 78,12);// arguments
