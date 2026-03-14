f3dded8e77e9260e20ba58317399e7dac31c83af
SultanAli41 <Sultanalibalouch512@gmail.com>
2026-03-14 23:49:04 +0500

    Event Listner Practice

--- EventListner.js
+++ EventListner.js
@@ -10,9 +10,37 @@ p.addEventListener('dblclick',green);
 p.removeEventListener('dblclick',green)
 //----------------------------------
 //common events
-let inp = document.querySelector('input');
+//input
+let inp1 = document.querySelector('input');
 inp.addEventListener('input',function(event){
     if(event.data !== null){
      console.log(event.data);
 }
 })
+// change Event 
+let sel = document.querySelector('#sel');
+let h = document.querySelector('h3');
+sel.addEventListener('change',function(val){
+    h.textContent=`${val.target.value} Device Selected`;
+})
+//////-------------------------------------------------------
+let h1 = document.querySelector('#ex1');
+window.addEventListener('keydown',function(val){
+    if(val.key === " "){
+        h1.textContent="SPC";
+    }else{
+        h1.textContent=val.key;
+    }
+})
+//--------------------- upload button & change button name accordin to file name
+let btn = document.querySelector('#btn');
+let inp = document.querySelector('#inp');
+btn.addEventListener('click',function(){
+    inp.click();
+})
+inp.addEventListener('change',function(det){
+const files = det.target.files[0];
+if(files){
+    btn.textContent='files.name';
+}
+}); 
