4e47385f75dc23a35d7b7ddab851c809672add2f
SultanAli41 <Sultanalibalouch512@gmail.com>
2026-03-27 07:44:26 +0500

    Project 1

---
+++ SmallProject1.js
@@ -0,0 +1,45 @@
+let Users = [
+  {
+    name: "Ali Khan",
+    pic: "https://images.unsplash.com/photo-1554126807-6b10f6f6692a?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    bio: "Coffee lover, passionate frontend developer."
+  },
+  {
+    name: "Ahmed Raza",
+    pic: "https://images.unsplash.com/photo-1519456264917-42d0aa2e0625?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    bio: "Backend developer, Node.js enthusiast."
+  },
+  {
+    name: "Usman Tariq",
+    pic: "https://plus.unsplash.com/premium_photo-1661765722951-7c001baba43e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Ym95fGVufDB8fDB8fHww",
+    bio: "Full stack developer, MERN specialist."
+  },
+  {
+    name: "Hassan Ali",
+    pic: "https://images.unsplash.com/photo-1678557856807-7ae6ff6893d1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGJveXxlbnwwfHwwfHx8MA%3D%3D",
+    bio: "Creative UI designer, loves minimal design."
+  },
+  {
+    name: "Bilal Ahmed",
+    pic: "https://images.unsplash.com/photo-1608734265656-f035d3e7bcbf?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGJveXxlbnwwfHwwfHx8MA%3D%3D",
+    bio: "JavaScript expert and problem solver."
+  },
+  {
+    name: "Zain Malik",
+    pic: "https://plus.unsplash.com/premium_photo-1707816501228-1d814ad62d7b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGJveXxlbnwwfHwwfHx8MA%3D%3D",
+    bio: "Tech enthusiast, loves building web apps."
+  },
+  {
+    name: "Saad Khan",
+    pic: "https://images.unsplash.com/photo-1623170909888-4e97ff277186?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGJveXxlbnwwfHwwfHx8MA%3D%3D",
+    bio: "Software engineer with strong DSA skills."
+  }
+];
+function showUsers(arr){
+arr.forEach(function(Users){
+
+});
+}
+//Sare use show krwana
+//filter kanrna har bar input krnay pr 
+// filterd user show krna 
