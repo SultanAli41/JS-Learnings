b913f988dcdb892371c5cbc8773920f4621cf55f
SultanAli41 <Sultanalibalouch512@gmail.com>
2026-04-03 06:56:37 +0500

    Async JS

---
+++ async.js
@@ -0,0 +1,7 @@
+//Async JS --- Set timeout - set interval - promises -axios - fetch - XMLHTTPSRequest
+// Sync -- Simple Code -- Line by line 
+// Async Code ka main motive hoota hai jahan humain answer ka idea nhi hota kb response aay ga like facebook sy photo laoo
+console.log("Hey");
+setTimeout(function print(){
+    console.log("Hey2");
+},2000)
