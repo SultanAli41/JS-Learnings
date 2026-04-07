bf28f39b5959041c25e227bf29509bf86bc2315f
SultanAli41 <Sultanalibalouch512@gmail.com>
2026-04-07 07:34:01 +0500

    Update

--- async.js
+++ async.js
@@ -5,3 +5,36 @@ console.log("Hey");
 setTimeout(function print(){
     console.log("Hey2");
 },2000)
+// ek main stack hai aur ek side stack main stack mai sara sync code aa gaya aur side stack mai async code 
+// pehly snc chal jay ga aur side stack mai async chal raha hoo ga jesy hi wo complete hoo ga event loop usy main stack mai ly aay gi
+//--------------------Request Send 
+// fetch
+//axios
+//promises
+//settimeout
+//setinterval
+//--------------------Answer by 
+//then catch
+//callbacks 
+//async await
+
+//call back function -- will run after compleition of async code when we have to not run code immediatly 
+const ans = fetch("https//www.facebook.com");
+console.log(ans);
+// Promises -- Three States - Resolve  - Reject - Pending 
+let prom = new Promise((res,rej)=>{
+    let n = Math.floor(Math.random()*10);
+if(n<5){
+  res();
+}else{
+ rej();
+}
+});
+prom.then(
+    function(){
+        console.log("Below");
+    }).catch(
+    function(){
+       console.log("above");
+    }
+)
